b428960b6d970fc99f23e05197deda34af7f8599
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-03-05 18:59:40 -0800

    exercise 4.4
    -have working tests for EventList
    -created CitySearch component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <EventList />
     </div>
   );
 }
