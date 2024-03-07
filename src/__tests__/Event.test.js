5c7529d75ddc60d8b3b2b32b05da0ad25598dba7
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-03-06 19:49:03 -0800

    Feature 2 test suite + Event.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Event from '../components/Event';
+import mockData from '../mock-data';
+
+describe('<Event />', () => {
+    let EventComponent, detailsButton;
+    const event = mockData[0];
+
+    beforeEach(() => {
+        EventComponent = render(<Event event={event} />);
+        detailsButton = EventComponent.getByText('Show Details');
+    });
+
+    test('displays the event summary', () => {
+        expect(EventComponent.getByText(event.summary)).toBeInTheDocument();
+    });
+
+    test('displays the event creation time', () => {
+        expect(EventComponent.getByText(new Date(event.created).toString())).toBeInTheDocument();
+    });
+
+    test('displays the event location', () => {
+        expect(EventComponent.getByText(event.location)).toBeInTheDocument();
+    });
+
+    test('has a show details button', () => {
+        expect(detailsButton).toBeInTheDocument();
+    });
+
+    test('expands to show details when clicked', async () => {
+        await userEvent.click(detailsButton);
+    });
+
+    test('collapses to hide details when clicked again', async () => {
+        await userEvent.click(detailsButton);
+        await userEvent.click(detailsButton);
+    });
+});
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,12 @@
-
-
-const Event = () => {
+const Event = ({ event }) => {
     return (
-      <li></li>
+        <li>
+            <h1>{event.summary}</h1>
+            <p>{new Date(event.created).toString()}</p>
+            <p>{event.location}</p>
+            <button>Show Details</button>
+        </li>
     );
-  }
-  
-  export default Event;
+}
+
+export default Event;
