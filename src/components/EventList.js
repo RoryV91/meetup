83afcd603d58b5d90b45fb843da5ccea1f89a5a0
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-03-12 23:05:38 -0700

    Update EventList.js

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,18 +1,20 @@
 import Event from "./Event";
 
 const EventList = ({ events }) => {
-	return (
-		<ul id="event-list">
-			{events
-				? events.map((event) => (
-						<Event
-							key={event.id}
-							event={event}
-						/>
-				  ))
-				: null}
-		</ul>
-	);
+  return (
+    <ul id="event-list">
+      {events && events.length > 0 ? (
+        events.map((event) => (
+          <Event
+            key={event.id}
+            event={event}
+          />
+        ))
+      ) : (
+        <li id="no-events-li">No Events!</li>
+      )}
+    </ul>
+  );
 };
 
 export default EventList;
