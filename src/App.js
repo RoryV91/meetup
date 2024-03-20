b17d1b047c1455097964795f41cd4bd3d3f51c37
Rory Van Blaricom <115426977+RoryV91@users.noreply.github.com>
2024-03-20 00:15:25 -0700

    warning alert/offline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 import "./App.css";
 
@@ -14,8 +14,15 @@ const App = () => {
 	const [currentCity, setCurrentCity] = useState("See all cities");
 	const [infoAlert, setInfoAlert] = useState("");
 	const [errorAlert, setErrorAlert] = useState("");
+	const [warningAlert, setWarningAlert] = useState("");
 
 	useEffect(() => {
+		if (!navigator.onLine) {
+			setWarningAlert("You are currently offline. Some features may not be available.");
+		} else {
+			setWarningAlert("");
+		}
+
 		fetchData();
 	}, [currentCity, currentNOE]);
 
@@ -34,6 +41,7 @@ const App = () => {
 			<div className="alerts-container">
 				{infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
 				{errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+				{warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
 			</div>
 			<h1 id="title">meetup.</h1>
 			<h2>The place to go, to find places to go and meet people.</h2>
